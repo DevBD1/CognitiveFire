06e80db80505c357ac71069ab00d0ab4ba322c2a
DevBD1 <burakdorman@gmail.com>
2025-10-27 17:45:58 +0300

    finalized the cross&aim for now

--- Assets/Project/ThirdPersonController/Scripts/ThirdPersonShooterController.cs
+++ Assets/Project/ThirdPersonController/Scripts/ThirdPersonShooterController.cs
@@ -3,6 +3,7 @@
 using UnityEngine;
 using Cinemachine;
 using CognitiveFire;
+using UnityEngine.InputSystem;
 
 public class ThirdPersonShooterController : MonoBehaviour
 {
@@ -10,6 +11,8 @@ public class ThirdPersonShooterController : MonoBehaviour
     [SerializeField] private GameObject crossair;
     [SerializeField] private float normalSensitivity;
     [SerializeField] private float aimingSensitivity;
+    [SerializeField] private LayerMask aimColliderLayerMask = new LayerMask();
+    [SerializeField] private Transform debugTransform;
 
     private ThirdPersonController thirdPersonController;
     private CognitiveFireInputs cognitiveFireInputs;
@@ -22,12 +25,31 @@ public class ThirdPersonShooterController : MonoBehaviour
 
     private void Update()
     {
+        Vector3 mouseWorldPosition = Vector3.zero;
+
+        Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
+        Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, aimColliderLayerMask))
+        {
+            debugTransform.position = raycastHit.point;
+            //Vector3 aimTarget = raycastHit.point;
+            //Debug.DrawLine(ray.origin, aimTarget, Color.red);
+            mouseWorldPosition = raycastHit.point;
+        }
+
         if (cognitiveFireInputs.aim)
         {
             shooterCamera.gameObject.SetActive(true);
             shooterCamera.Priority = 20; // Higher priority to switch to shooter camera
             thirdPersonController.SetSensitivity(aimingSensitivity);
             crossair.SetActive(true); // Show crossair when aiming
+            thirdPersonController.SetRotateOnMove(false);
+
+            Vector3 worldAimTarget = mouseWorldPosition;
+            worldAimTarget.y = transform.position.y;
+            Vector3 aimDirection = (worldAimTarget - transform.position).normalized;
+
+            transform.forward = Vector3.Lerp(transform.forward, aimDirection, Time.deltaTime * 20f);
         }
         else
         {
@@ -35,6 +57,7 @@ public class ThirdPersonShooterController : MonoBehaviour
             shooterCamera.Priority = 0; // Lower priority to switch back to default camera
             thirdPersonController.SetSensitivity(normalSensitivity);
             crossair.SetActive(false); // Hide crossair when not aiming
+            thirdPersonController.SetRotateOnMove(true);
         }
     }
 }
--- README.md
+++ README.md
@@ -5,4 +5,4 @@ Third Person Shooter in Unity 6.2
 ## Features
 
 - ThirdPersonController: We used Unity's StarterAssets for basic controls and locomotives
-- Aiming: PlayerAimCamera, AimSensitivity
+- Aiming: PlayerAimCamera, AimSensitivity, crosshair only active when the player aims, rotate the player's face when moving the aim, prevent the character from rotation while aiming
