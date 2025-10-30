dafad12d8e96ba61e68b28c8f5d25cb6b2625983
DevBD1 <burakdorman@gmail.com>
2025-10-30 15:47:34 +0300

    add optional shooting mode (Raycast or Projectile) for player attacks
    switch with M key

--- Assets/Project/Scripts/ThirdPersonShooterController.cs
+++ Assets/Project/Scripts/ThirdPersonShooterController.cs
@@ -5,8 +5,15 @@ using Cinemachine;
 using CognitiveFire;
 using UnityEngine.InputSystem;
 
+public enum ShootingMode
+{
+    Raycast,
+    Projectile
+}
+
 public class ThirdPersonShooterController : MonoBehaviour
 {
+
     [SerializeField] private CinemachineVirtualCamera shooterCamera;
     [SerializeField] private GameObject crossair;
     [SerializeField] private float normalSensitivity;
@@ -16,6 +23,12 @@ public class ThirdPersonShooterController : MonoBehaviour
     [SerializeField] private Transform debugTransform;
     [SerializeField] private Transform bpj; //pfBulletProjectile
     [SerializeField] private Transform sbp; //spawnBulletPosition
+
+    [SerializeField] private Transform vfxHitG;
+    [SerializeField] private Transform vfxHitR;
+
+    [SerializeField] private ShootingMode shootingMode = ShootingMode.Raycast;
+
 
     private Animator animator;
 
@@ -31,16 +44,25 @@ public class ThirdPersonShooterController : MonoBehaviour
 
     private void Update()
     {
+        if (Keyboard.current.mKey.wasPressedThisFrame)
+        {
+            shootingMode = shootingMode == ShootingMode.Raycast ? ShootingMode.Projectile : ShootingMode.Raycast;
+            Debug.Log($"Shooting Mode: {shootingMode}");
+        }
+
         Vector3 mouseWorldPosition = Vector3.zero;
 
         Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
         Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
+        //Ray ray = new Ray(sbp.position, Camera.main.transform.forward);
+        Transform hitTransform = null;
         if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, aimColliderLayerMask))
         {
             debugTransform.position = raycastHit.point;
             //Vector3 aimTarget = raycastHit.point;
             //Debug.DrawLine(ray.origin, aimTarget, Color.red);
             mouseWorldPosition = raycastHit.point;
+            hitTransform = raycastHit.transform;
         }
 
         if (cognitiveFireInputs.aim)
@@ -70,8 +92,30 @@ public class ThirdPersonShooterController : MonoBehaviour
 
         if (cognitiveFireInputs.shoot)
         {
-            Vector3 aimDir = (mouseWorldPosition - sbp.position).normalized;
-            Instantiate(bpj, sbp.position, Quaternion.LookRotation(aimDir, Vector3.up));
+            // --- Raycast Mode ---
+            if (hitTransform != null)
+            {
+                Debug.Log($"Ray hit {hitTransform.name}");
+                Vector3 hitPoint = raycastHit.point;
+                Quaternion hitRot = Quaternion.LookRotation(raycastHit.normal);
+
+                if (hitTransform.GetComponent<BulletTarget>() != null)
+                {
+                    // Target hit program logic
+                    Instantiate(vfxHitG, hitPoint, hitRot);
+                }
+                else
+                {
+                    // Non-target hit program logic
+                    Instantiate(vfxHitR, hitPoint, hitRot);
+                }
+            }
+            if (shootingMode == ShootingMode.Projectile)
+            {
+                // --- Projectile Mode ---
+                Vector3 aimDir = (mouseWorldPosition - sbp.position).normalized;
+                Instantiate(bpj, sbp.position, Quaternion.LookRotation(aimDir, Vector3.up));
+            }
             cognitiveFireInputs.shoot = false;
         }
     }
