6b993c1358f233277d81f302f964dc0a6a9b7cfe
DevBD1 <burakdorman@gmail.com>
2025-11-02 11:07:55 +0300

    created _initialize script as a core

---
+++ Assets/Project/Scripts/_initialize.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class MainScript : MonoBehaviour
+{
+    // Awake is called when the script instance is being loaded
+    private void Awake()
+    {
+        Debug.Log("CognitiveFire is awakening.");
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("CognitiveFire is starting.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Called every fixed framerate frame
+    private void FixedUpdate()
+    {
+        
+    }
+
+    // Called every frame after all Update functions have been called
+    private void LateUpdate()
+    {
+        
+    }
+}
