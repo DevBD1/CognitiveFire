076ce8a348f73cb9d85e29ae3ed5dfdd85733dcf
DevBD1 <burakdorman@gmail.com>
2025-11-03 20:11:54 +0300

    started to work on the sci-fi concept level

--- Assets/Project/Scripts/_initialize.cs
+++ Assets/Project/Scripts/_initialize.cs
@@ -1,33 +1,38 @@
 using UnityEngine;
 
-public class MainScript : MonoBehaviour
+namespace CognitiveFire
 {
-    // Awake is called when the script instance is being loaded
-    private void Awake()
+    // This namespace can be used to group related classes and avoid naming conflicts
+    public class MainScript : MonoBehaviour
     {
-        Debug.Log("CognitiveFire is awakening.");
-    }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        Debug.Log("CognitiveFire is starting.");
+        // Awake is called when the script instance is being loaded
+        private void Awake()
+        {
+            Debug.Log("CognitiveFire is awakening.");
+        }
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        private void Start()
+        {
+            Debug.Log("CognitiveFire is starting.");
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+
+        }
+
+        // Called every fixed framerate frame
+        private void FixedUpdate()
+        {
+
+        }
+
+        // Called every frame after all Update functions have been called
+        private void LateUpdate()
+        {
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    // Called every fixed framerate frame
-    private void FixedUpdate()
-    {
-        
-    }
-
-    // Called every frame after all Update functions have been called
-    private void LateUpdate()
-    {
-        
-    }
 }
