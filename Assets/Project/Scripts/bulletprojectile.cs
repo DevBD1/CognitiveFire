68655c88fcc8419d4d946cd85528ab1f9eef4e3c
sertcozdemir <sertac_ozdemir41@hotmail.com>
2025-11-07 16:13:40 +0300

    mermı olayı

--- Assets/Project/Scripts/bulletprojectile.cs
+++ Assets/Project/Scripts/bulletprojectile.cs
@@ -5,17 +5,26 @@ using Unity.Mathematics;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Ilumisoft.HealthSystem;
+
 
 public class BulletProjectile : MonoBehaviour
 {
     [SerializeField] private Transform vfxHitG;
     [SerializeField] private Transform vfxHitR;
     private Rigidbody brigidbody;
+    private Transform owner;
+    [SerializeField] private int damage = 20;
+private Collider myCol;
 
     private void Awake()
     {
         brigidbody = GetComponent<Rigidbody>();
+        myCol = GetComponent<Collider>();
+        
     }
+    public void Initialize(Transform owner) { this.owner = owner; if (myCol == null) myCol = GetComponent<Collider>(); foreach (var c in owner.GetComponentsInChildren<Collider>()) { if (c) Physics.IgnoreCollision(myCol, c, true); } }
+
     private void Start()
     {
         float speed = 40f;
@@ -29,11 +38,13 @@ public class BulletProjectile : MonoBehaviour
             target.TakeDamage(25);
             // Target hit program logic
             Instantiate(vfxHitG, transform.position, Quaternion.identity);
+            Destroy(gameObject);
         }
         else
         {
             // Non-target hit program logic
             Instantiate(vfxHitR, transform.position, Quaternion.identity);
+            Destroy(gameObject);
         }
         Destroy(gameObject);
     }
